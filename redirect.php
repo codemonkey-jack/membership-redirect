8453dc00309e97d8b85520cabead414ecaa5af34
Jack Kitterhing <jack@incsub.com>
2014-10-20 19:57:43 +0100

    Updated site URL
    #Update registration URL redirect

--- redirect.php
+++ redirect.php
@@ -4,7 +4,7 @@ add_action('init', 'redirect_to_registration', 0);
 
 function redirect_to_registration() {
    if (isset($_GET['action']) && $_GET['action'] == 'validatepage1') {
-       wp_redirect(site_url('/portal-registration/'));
+       wp_redirect(site_url('/registration-complete/'));
        exit;
    }
 }
