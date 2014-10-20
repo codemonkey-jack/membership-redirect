a5988af29636a1ea97ed1ded1e85feaebe69805a
Jack Kitterhing <jack@incsub.com>
2014-10-20 20:27:33 +0100

    Added comment.

--- redirect.php
+++ redirect.php
@@ -2,6 +2,7 @@
 
 add_action('init', 'redirect_to_registration', 0);
 
+// Create function
 function redirect_to_registration() {
    if (isset($_GET['action']) && $_GET['action'] == 'validatepage1') {
        wp_redirect(site_url('/registration-complete/'));
